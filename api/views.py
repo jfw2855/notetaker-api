d3e32fde01d25401fd32de906bbce609683c2767
jfw2855 <jwaddle1130@gmail.com>
2022-05-12 14:30:27 -0500

    adds get route for all notesin db

--- api/views.py
+++ api/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+#for initial setup testing
+#from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 
-# Create your views here.
+
+# for initial setup testing
+# def getRoutes(request):
+#     return JsonResponse('test works',safe=False)
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    return Response(routes)
+
+#Gets all note data
+@api_view(['GET'])
+def getNotes(request):
+    #grabs all notes form psql
+    notes = Note.objects.all()
+    #serializes the python dicts into JSON
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
