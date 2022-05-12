8a3cec8e597640d3f474f9c7baeacb8be418ecd1
jfw2855 <jwaddle1130@gmail.com>
2022-05-11 23:22:19 -0500

    adds a note model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    body = models.TextField(null=True,blank=True)
+    updated = models.DateTimeField(auto_now=True) #auto_now takes a timestamp for each time saved
+    created = models.DateTimeField(auto_now_add=True) #auto_now_add only takes a timestamp from intial creation
+
+    def __str__(self):
+        #only grabs first 60 characters to display
+        return self.body[0:60]
